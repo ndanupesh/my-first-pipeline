8c82390d9daa8beb2522b64fdc8ecd7eb06246ba
ndanupesh <113617254+ndanupesh@users.noreply.github.com>
2026-01-23 10:45:31 +0300

    Integrate Flask and modify add function usage
    Refactor app.py to use Flask and display calculation result.

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
+from flask import Flask
+from app import add 
+
+app = Flask(__name__)
+
 def add(a, b):
     return a + b
 
+@app.route('/')
+def home():
+    //What the user will see in the browser
+    result = add(10, 20)
+    return f"<h1>Hello from the Dev Side!</h1><p>The calculation is: {result}</p>"
+
 if __name__ == "__main__":
-    print("Running app...")
-    print(f"2 + 3 = {add(2, 3)}")
+    #'0.0.0.0 means 'listen to everyone', not just local connections
+    app.run(host='0.0.0.0', port=5000)
+    
